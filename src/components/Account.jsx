8f62219b68c3f1cc240526174bdda5c3dd554be8
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-07-05 10:33:49 +0200

    commentaries + css

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,22 +2,25 @@ import PropTypes from 'prop-types';
 
 /**
  * A React functional component that displays account information.
- *
  * @param {Object} props
  * @param {Object} props.account
  * @param {string} props.account.title
  * @param {number} props.account.amount
- * @param {string} props.account.amountDescription
+ * @param {string} props.account.description
  * @returns {JSX.Element}
  */
 const Account = ({ account }) => {
-  const { title, amount, amountDescription } = account;
+  const { title, amount, description } = account;
+  // for phase 2 : we may create a function 'openTransactions' with accountId as param
+  // this function will be set to the button
+  // this function could call an async function of slicer.
+  // the endpoint would look something like this : /accounts/{idAccount}/transactions/{currentMonth}
   return (
     <section className='account'>
       <div className='account__content-wrapper'>
         <h3 className='account__title'>{title}</h3>
         <p className='account__amount'>${amount.toLocaleString('en-US')}</p>
-        <p className='account__amount-description'>{amountDescription}</p>
+        <p className='account__description'>{description}</p>
       </div>
       <div className='account__content-wrapper cta'>
         <button className='account__transaction-button'>
@@ -31,7 +34,7 @@ Account.propTypes = {
   account: PropTypes.shape({
     title: PropTypes.string,
     amount: PropTypes.number,
-    amountDescription: PropTypes.string,
+    description: PropTypes.string,
   }).isRequired,
 };
 export default Account;
--- src/utils/slicerFunctions.js
+++ src/utils/slicerFunctions.js
@@ -1,5 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+/**
+ * Creates an async thunk action creator using `createAsyncThunk` from Redux Toolkit.
+ * @param {string} type - The type of the async thunk action.
+ * @param {function} apiFunction - The API function to be called asynchronously.
+ * @returns {function} An async thunk action creator.
+ */
 export const createThunkAction = (type, apiFunction) => {
   return createAsyncThunk(type, async (params) => {
     const response = await apiFunction(params);
@@ -7,6 +13,14 @@ export const createThunkAction = (type, apiFunction) => {
   });
 };
 
+/**
+ * Handles async action states (pending, fulfilled, rejected) and updates the state accordingly.
+ * Uses in extra reducers slicer
+ * @param {object} builder - The Redux Toolkit `builder` object to add case reducers.
+ * @param {object} thunk - The async thunk action object containing `pending`, `fulfilled`, and `rejected` actions.
+ * @param {string} slicer - The key in the state object where the data slice will be stored.
+ * @param {string} statusKey - The key in the state object where the status (loading, succeeded, failed) will be stored.
+ */
 export const handleAsyncActions = (builder, thunk, slicer, statusKey) => {
   builder
     .addCase(thunk.pending, (state) => {
